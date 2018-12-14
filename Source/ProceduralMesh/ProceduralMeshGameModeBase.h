ba1e2eee51fbdce93c1d7720c880258ef7d67f40
yanglixiao <yanglixiao1994@gmail.com>
2018-12-14 14:45:22 +0800

    Initial commit

---
+++ Source/ProceduralMesh/ProceduralMeshGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralMeshGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALMESH_API AProceduralMeshGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
